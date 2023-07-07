105988c7127ab9bf560b0db351df5c9e5c832b25
Dr. Raed <drraed.jb@gmail.com>
2023-07-07 18:44:11 +0200

    adding a tabbar with two tabs

--- lib/hauptseiteforapp.dart
+++ lib/hauptseiteforapp.dart
@@ -12,10 +12,47 @@ class _HauptSeiteState extends State<HauptSeite> {
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
+      initialIndex: 0,
       child: Scaffold(
         appBar: AppBar(
-          title: Text("Some of NP complete problems"
+          title: const Text("Some of NP complete problems"
           ),
+          bottom: const TabBar(tabs: 
+          [
+            Tab(
+              text: "NP complete problems",
+            ),
+            Tab(
+              text: "refences",
+            ),
+          ]),
+          ),
+          body: TabBarView(
+            children: [
+              Container(
+                height: 400,
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: 
+                    [
+                      Colors.pink,
+                      Colors.white
+                    ])
+                ),
+              ),
+              Container(
+                  decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: 
+                    [
+                      
+                      Colors.white,
+                      Colors.pink
+                    ])
+                ),
+             
+              )
+            ],
           ),
       ),
     );
