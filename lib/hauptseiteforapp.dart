f949c8a261bfba5efd0655a341d0db5030f02059
Dr. Raed <drraed.jb@gmail.com>
2023-07-07 19:11:30 +0200

    add  two buttons

--- lib/hauptseiteforapp.dart
+++ lib/hauptseiteforapp.dart
@@ -8,6 +8,42 @@ class HauptSeite extends StatefulWidget {
 }
 
 class _HauptSeiteState extends State<HauptSeite> {
+  final List _listOfNPcompleteProblems=[
+  "SAT",
+  "maximum clique",
+  "minimum vertex cover",
+  "Hamiltonian cycle problem",
+  "longest path problem",
+  "Hamiltonian path problem",
+  "3SAT",
+  "Subset Sum",
+  "partition problem",
+  "longest cycle problem",
+  "Traveling Salesman problem",
+];
+// ignore: prefer_final_fields
+void nextProblem()
+{
+  setState(() {
+    if (_indexPrpblem<_listOfNPcompleteProblems.length-1) 
+    {
+         _indexPrpblem++;
+    }
+    
+  });
+}
+
+void previousProblem()
+{
+  setState(() {
+    if (_indexPrpblem>1) 
+    {
+         _indexPrpblem--;
+    }
+    
+  });
+}
+int _indexPrpblem=0;
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -30,6 +66,8 @@ class _HauptSeiteState extends State<HauptSeite> {
           body: TabBarView(
             children: [
               Container(
+                padding: EdgeInsets.all(24),
+                margin: EdgeInsets.all(4),
                 height: 400,
                 decoration: const BoxDecoration(
                   gradient: LinearGradient(
@@ -39,6 +77,29 @@ class _HauptSeiteState extends State<HauptSeite> {
                       Colors.white
                     ])
                 ),
+
+                child: Column(
+              children: [
+                Text(_listOfNPcompleteProblems[_indexPrpblem])
+              ,
+              Center(
+                child: Row(
+                  children: [
+                    TextButton(onPressed: (){
+                  previousProblem();
+                }, child: Icon(Icons.navigate_before)
+                    ),              
+                  
+                TextButton(
+                  onPressed: (){
+                       nextProblem();
+                }, child: Icon(Icons.navigate_next)
+                )
+                  ],
+                ),
+              )
+              ],
+             ),
               ),
               Container(
                   decoration: const BoxDecoration(
